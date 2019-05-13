3f00c166ed6f57b6b85a00ec3b17c1575e971d8d
Johan Carlsson <johan.axel.carlsson@gmail.com>
2019-05-13 09:52:52 +0200

    test of observer

--- src/se/kth/iv1350/processSale/view/TotalRevenueView.java
+++ src/se/kth/iv1350/processSale/view/TotalRevenueView.java
@@ -6,15 +6,27 @@ import se.kth.iv1350.processSale.model.RevenueObserver;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * View implementing the <code>RevenueObserver</code> interface. Responsible of presenting a running count of the
+ * revenue from program start
+ */
 public class TotalRevenueView implements RevenueObserver {
     private List<Money> revenue = new ArrayList<Money>();
 
+    /**
+     * Method to be called by observed class when new payment have been made
+     *
+     * @param revenue payment minus change
+     */
     @Override
     public void newPayment(Money revenue){
         this.revenue.add(revenue);
         printTotalRevenue();
     }
 
+    /*
+     * Sum and print list of payments
+     */
     private void printTotalRevenue() {
         Money totalRevenue = new Money();
         for (Money payment : revenue){
---
+++ test/se/kth/iv1350/processSale/view/TotalRevenueViewTest.java
@@ -0,0 +1,30 @@
+package se.kth.iv1350.processSale.view;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import se.kth.iv1350.processSale.model.Money;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TotalRevenueViewTest {
+
+    TotalRevenueView view;
+
+    @BeforeEach
+    void setup(){
+        view = new TotalRevenueView();
+    }
+
+    @AfterEach
+    void teardown(){
+        view = null;
+    }
+
+    @Test
+    void testNewPayment() {
+        view.newPayment(new Money(300));
+        String expPrint = "TotalRevenueView:: Total revenue: 300";
+
+    }
+}
